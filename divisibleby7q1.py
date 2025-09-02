74517d7cbe0653e2799dc97b404617540fd5fab6
~ANJALI~ <anjalikumari15243@gmail.com>
2025-09-01 22:01:31 -0500

    Update divisibleby7q1.py

--- divisibleby7q1.py
+++ divisibleby7q1.py
@@ -1,8 +1,6 @@
 
-number = int(input("Enter a number: "))
-
-
-if number % 7 == 0:
-    print(f"{number} is divisible by 7.")
+num = int(input("Enter a no. : "))
+if(num % 7 == 0):
+    print("num =",num,"is divisible by 7")
 else:
-    print(f"{number} is not divisible by 7.")
+    print("num =",num,"is NOT divisible by 7")
